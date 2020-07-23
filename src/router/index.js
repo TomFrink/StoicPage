0d11a30ee371c63d39e6c50696f2d96c07933685
TomFrink <tomfrink11@hotmail.com>
2020-07-22 22:33:56 -0400

    Added Admin Page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Books from "../views/Books.vue";
 import About from "../views/About.vue";
+import Admin from "../views/Admin.vue";
 
 Vue.use(VueRouter);
 
@@ -10,25 +11,30 @@ const routes = [
   {
     path: "/",
     meta: {
-      title: "Stoic"
+      title: "Stoic",
     },
     name: "home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
     name: "about",
-    component: About
+    component: About,
   },
   {
     path: "/books",
     name: "books",
-    component: Books
-  }
+    component: Books,
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: Admin,
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Admin.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>Admin Section</h1>
+    <section>
+      <input v-model="person" placeholder="Person" />
+      <br />
+      <textarea v-model="quote" placeholder="Quote"></textarea>
+      <p>
+        <i>Person is:</i>
+        <b>{{ person }}</b>
+      </p>
+      <span>
+        <i>Quote is:</i>
+      </span>
+      <p style="white-space: pre-line;">
+        <b>{{ quote }}</b>
+      </p>
+      <button onclick="addQuote">Add Quote</button>
+    </section>
+    <section>
+      <ul id="quotes">
+        <li v-for="thing in things" :key="thing.quote">
+          {{ thing.time.seconds | dater }} - {{ thing.quote }} by
+          {{ thing.person }}
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import { db } from "@/db";
+
+export default {
+  name: "Admin",
+  filters: {
+    dater: function(value) {
+      if (!value) return "";
+      value = new Date(value * 1000).toDateString();
+      return value;
+    },
+  },
+  data() {
+    return {
+      things: [],
+      person: "",
+      quote: "",
+    };
+  },
+  firestore() {
+    return {
+      things: db.collection("quotes").orderBy("time", "desc"),
+    };
+  },
+  methods: {
+    addQuote(person, quote) {
+      const time = new Date();
+      db.collection("quotes").add({ person, quote, time });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
